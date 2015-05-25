329a2f2993688c04b601f2d6005147264451c740
Lars Wikman <lars.wikman@lineducation.se>
2015-05-25 13:27:40 +0200

    Blocky spiral.

--- script.js
+++ script.js
@@ -43,18 +43,51 @@ function run() {
     frame++;
 }
 
-function p() {
-    pixel_data[0] = 0;
-    pixel_data[1] = 0;
-    pixel_data[2] = 0;
-    pixel_data[3] = 255;
-    pixel_context.putImageData( pixel_id, frame, frame );
+function p(rgba, position) {
+    console.log(rgba, position);
+    pixel_data[0] = rgba[0];
+    pixel_data[1] = rgba[1];
+    pixel_data[2] = rgba[2];
+    pixel_data[3] = rgba[3];
+    pixel_context.putImageData( pixel_id, position[0], position[1]);
 }
 
 a(0, console.log, console, ['zero']);
 a(500, console.log, console, ['fuck yeah!']);
-for(i = 0; i < 640; i++) {
-    a(i, p, window);
+var current_limit = 128;
+var current_count = 0;
+var direction = 0;
+var x = 0;
+var y = 0;
+for(i = 0; i < 2048; i++) {
+    switch(direction) {
+        case 0:
+            x++;
+        break;
+        case 1:
+            y++;
+        break;
+        case 2:
+            x--;
+        break;
+        case 3:
+            y--;
+        break;
+        default:
+            x++;
+    }
+    if(current_count > current_limit) {
+        current_count = 0;
+        current_limit -= 8;
+        if(direction < 3) {
+            direction++;
+        } else {
+            direction = 0;
+        }
+    }
+    current_count++;
+
+    a(i, p, window, [[0,0,0,255], [x,y]]);
 }
 
 window.requestAnimationFrame(run);
